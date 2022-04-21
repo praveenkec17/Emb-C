9f6a232d7f2935c5307d8bf4c476fc1adf62eaa8
PRAVEENKUMAR K <88073170+praveenkec17@users.noreply.github.com>
2022-04-21 10:08:32 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -3,42 +3,42 @@
 
 int main()
 {
-  DDRC=0XFF;
+  DDRC=0XFF; // set all 8 bits to  output//
   while(1)
   {
     PORTC=0b00111111;
-    _delay_ms(1000);
+    _delay_ms(1000);  //delay 1000ms
 
     PORTC=0b00000110;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
     PORTC=0b01011011;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
     PORTC=0b01001111;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
     PORTC=0b01100110;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
     
     PORTC=0b01101101;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000 ms delay
 
     PORTC=0b11111101;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
     
     PORTC=0b00000111;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
 
     PORTC=0B01111111;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
 
     PORTC=0b11101111;
-    _delay_ms(1000);
+    _delay_ms(1000); //1000ms delay
 
   }
 
